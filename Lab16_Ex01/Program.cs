7af985728c2bebb04b8cb4483474fdfe0192a027
SadPao0102 <thanyathep.hankla@gmail.com>
2025-03-31 23:24:35 +0700

    66030243ธัญเทพ

---
+++ Lab16_Ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿﻿BaseClass BC = new DerivedClass();
+BC.A(); 
+
+abstract class BaseClass
+{
+    abstract public void A();
+}
+class DerivedClass : BaseClass
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Implementation of inheritance classes");
+    }
+}
